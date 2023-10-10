08338e785355a6e6f67c2dd045c12aab3adcdcfe
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-10 17:01:38 -0400

    Link skills to Home component

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; 
 
 import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+    {
+      path: 'home',
+      component: HomeComponent
+    },
     {
       path: 'projects',
       component: ProjectsComponent
