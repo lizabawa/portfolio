5fd28a960a2159753361d7ceb6b536a0024d5c79
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-10 16:31:26 -0400

    Add data to Projects

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,1 +1,21 @@
-//add projects into this file
+interface Projects {
+    id: number;
+    title: string;
+    languages: string;
+    description: string;
+}
+
+export const projects: Projects[] = [
+    {
+        id: 1,
+        title: "Budgeteers",
+        languages: "Java",
+        description: "A budgeting API where a user can perform CRUD actions on expenses within a designated budget. Technologies Used: JWT, Spring Boot, Spring Security, MockMVC"
+    },
+    {
+        id: 0,
+        title: "Tic-Tac-Toe",
+        languages: "Javascript, HTML, CSS",
+        description: "A game of tic-tac-toe between two players, one whom is randomly chosen to start. Technologies Used: Bootstrap, SweetAlerts"
+    }
+]
