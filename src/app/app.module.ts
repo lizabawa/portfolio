ae2b82b547c762a046479b951d422e71f7d740d3
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-10 17:19:43 -0400

    Add tagline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { ProjectsComponent } from './projects/projects.component';
 import { AppRoutingModule } from './app-routing-module';
 import { SkillsComponent } from './skills/skills.component';
 import { HomeComponent } from './home/home.component';
+import { TaglineComponent } from './tagline/tagline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent,
     SkillsComponent,
-    HomeComponent
+    HomeComponent,
+    TaglineComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tagline/tagline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tagline',
+  templateUrl: './tagline.component.html',
+  styleUrls: ['./tagline.component.css']
+})
+export class TaglineComponent {
+
+}
