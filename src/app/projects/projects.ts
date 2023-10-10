92ee1a1573b6186a66bad3f97c38c12da8555b65
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-10 19:18:54 -0400

    Add github and img links to project component

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -3,18 +3,24 @@ interface Projects {
     title: string;
     languages: string;
     description: string;
+    github: string;
+    img: any;
 }
 export const projects: Projects[] = [
     {
         id: 1,
         title: "Budgeteers",
         languages: "Java",
-        description: "A budgeting API where a user can perform CRUD actions on expenses within a designated budget. Technologies Used: JWT, Spring Boot, Spring Security, MockMVC"
+        description: "A budgeting API where a user can perform CRUD actions on expenses within a designated budget. Technologies Used: JWT, Spring Boot, Spring Security, MockMVC",
+        github: "https://github.com/lizabawa/budgeteers-server",
+        img: "https://github.com/lizabawa/budgeteers-server/raw/main/endpoints.png"
     },
     {
         id: 0,
         title: "Tic-Tac-Toe",
         languages: "Javascript, HTML, CSS",
-        description: "A game of tic-tac-toe between two players, one whom is randomly chosen to start. Technologies Used: Bootstrap, SweetAlerts"
+        description: "A game of tic-tac-toe between two players, one whom is randomly chosen to start. Technologies Used: Bootstrap, SweetAlerts",
+        github: "https://github.com/lizabawa/tic-tac-toe",
+        img: "https://github.com/lizabawa/tic-tac-toe/raw/main/media/ttt-ui.png"
     }
 ]
