7708a8c7c481902ae989ce36e2fc76281e1e513f
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-11 20:26:41 -0400

    Add social media icon links and remove contact button

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,20 +7,5 @@ import { projects } from './projects';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
-  project: any;
-
-  constructor(private route: ActivatedRoute) {
-
-  }
-
-  ngOnInit() {
-      this.route.paramMap
-      .subscribe(params => {
-        this.project = projects.find( project => {
-          let paramId: string = params.get('id') || ''
-          return project.id === parseInt(paramId)
-        })
-      })
-  }
+export class ProjectsComponent {
 }
